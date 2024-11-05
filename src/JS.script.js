b4553fb950606c22bb3262295784b30c59ff6f6a
Isabella Ferreira da Silva <161853096+IsabellaFerreira03@users.noreply.github.com>
2024-11-05 11:22:03 -0300

    commit

--- src/JS.script.js
+++ src/JS.script.js
@@ -4,7 +4,7 @@
 // * para a multiplicação
 // / para a divisão real
 // % para a divisão inteira
-// ** ara achar o quadrado do numero.
+// ** para achar o quadrado do numero.
 
 // Um lembrete é que, uma ordem de operadores sempre será realizada primeiro, idependente dos que forem colocados primeiro, para os dar prioridade e manter a ordem desejada, deve os ccolocar em ()
 
@@ -14,5 +14,5 @@
 
 // As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
 
-// E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Masd isso será visto e aprendido melhor mais para frente.
+// E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Mas isso será visto e aprendido melhor mais para frente.
 
