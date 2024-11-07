c919b5c8a8d747e20d5d63a49ca803cd3513ba14
Isabella Ferreira da Silva <161853096+IsabellaFerreira03@users.noreply.github.com>
2024-11-07 11:33:39 -0300

    commit

--- src/JS.script.js
+++ src/JS.script.js
@@ -1,4 +1,4 @@
-// Os operadores usados em JS são;
+// OS OPOERADORES NUMERICOS EM JS;
 // + para a soma
 // - menos para a subtração
 // * para a multiplicação
@@ -7,12 +7,49 @@
 // ** para achar o quadrado do numero.
 
 // Um lembrete é que, uma ordem de operadores sempre será realizada primeiro, idependente dos que forem colocados primeiro, para os dar prioridade e manter a ordem desejada, deve os ccolocar em ()
-
 // A ordem de precedência dos operadores sempre será: () , **, 
 //  * , / , % , tem a mesma precendência, nesse caso, quem aparecer primeiro, será feito primeiro.
 // e por último, ele fará + e -
 
-// As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
 
+// OPERADORES ATRIBUIÇÃO
+// As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
 // E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Mas isso será visto e aprendido melhor mais para frente.
 
+
+// O comadno typeof mostra qual o tipo do dado que você quer ver.
+
+
+// OPERADORES RELACIONAIS
+// São os famosos:
+// > = Maior
+// < = menor
+// >= = maior ou igual
+// <= = menor ou igual
+// == = igual
+// != = diferente
+
+// IDENTIDADE
+// === = é quando é o mesmo valor e tipo de dado. Se forem iguais ele mostrara 'true'
+// !== = esse é o desigual restrito, que mostra se os tipos são didferentes, ,es,o que tenham o mesmo valor. Se forem desiguais ele mostrata 'true' 
+
+// OPERADORES LÓGICOS
+// ! = negaçaõ (ou vai dar false ou true)
+// && = conjunção (Se tiver algum false na linha, será false)
+// || disjunção (se tive algum true na linnham será true.)
+
+// Se em uma linha forem colocados operadores aritmeticos, releacionais e lógicos, a ordem será, (aritmeticos, relacionais e lógicos.) 
+
+// E caso a ordem de execução da conta seja (!, &&, ||) será feita na ordem de !, &&, ||.
+
+
+// ORDEM DE PRECEDENCIA DESSA BAGAÇA TODA!!!
+// aritmeticos
+// relacionais
+// lógicos
+
+
+// TERNÁRIO
+// ?
+// :
+// Aqui podemos ver o conceito de 'VERDADEIRO' ou 'FALSO', onde o ? vai ser a verdadiro e o : falso. Tudo irá depende da questão para esse resultado. (Eu não entendi muito bem essa parte, mas espero entende com o tempo e experiencia.)
