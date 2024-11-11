31f1bc71d357e7d33830f047886e7b49311c6471
Isabella Ferreira da Silva <161853096+IsabellaFerreira03@users.noreply.github.com>
2024-11-11 10:45:11 -0300

    commit

--- src/JS.script.js
+++ src/JS.script.js
@@ -7,18 +7,25 @@
 // ** para achar o quadrado do numero.
 
 // Um lembrete é que, uma ordem de operadores sempre será realizada primeiro, idependente dos que forem colocados primeiro, para os dar prioridade e manter a ordem desejada, deve os ccolocar em ()
-// A ordem de precedência dos operadores sempre será: () , **, 
+
+// A ordem de precedência dos operadores sempre será: () , **
+
 //  * , / , % , tem a mesma precendência, nesse caso, quem aparecer primeiro, será feito primeiro.
+
 // e por último, ele fará + e -
 
+//_______________________________________________________________________
 
 // OPERADORES ATRIBUIÇÃO
 // As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
+
 // E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Mas isso será visto e aprendido melhor mais para frente.
 
+//_______________________________________________________________________
 
-// O comadno typeof mostra qual o tipo do dado que você quer ver.
+// MACETE: O comadno typeof mostra qual o tipo do dado que você quer ver.
 
+//_______________________________________________________________________
 
 // OPERADORES RELACIONAIS
 // São os famosos:
@@ -29,9 +36,15 @@
 // == = igual
 // != = diferente
 
-// IDENTIDADE
-// === = é quando é o mesmo valor e tipo de dado. Se forem iguais ele mostrara 'true'
-// !== = esse é o desigual restrito, que mostra se os tipos são didferentes, ,es,o que tenham o mesmo valor. Se forem desiguais ele mostrata 'true' 
+//_______________________________________________________________________
+
+// OPERADORES DE IDENTIDADE
+// === = é quando é o mesmo valor e tipo de dado. Se forem iguais ele mostrara 'true'. Exemplo, separar string de numero.
+
+// !== = esse é o desigual restrito, que mostra se os tipos são didferentes, oU que tenham o mesmo valor. Se forem desiguais ele mostrata 'true' como exemplo, separar string de numero.
+
+//_______________________________________________________________________
+
 
 // OPERADORES LÓGICOS
 // ! = negaçaõ (ou vai dar false ou true)
@@ -42,14 +55,18 @@
 
 // E caso a ordem de execução da conta seja (!, &&, ||) será feita na ordem de !, &&, ||.
 
+//_______________________________________________________________________
 
 // ORDEM DE PRECEDENCIA DESSA BAGAÇA TODA!!!
 // aritmeticos
 // relacionais
 // lógicos
 
+//_______________________________________________________________________
 
 // TERNÁRIO
 // ?
 // :
-// Aqui podemos ver o conceito de 'VERDADEIRO' ou 'FALSO', onde o ? vai ser a verdadiro e o : falso. Tudo irá depende da questão para esse resultado. (Eu não entendi muito bem essa parte, mas espero entende com o tempo e experiencia.)
+// Aqui podemos ver o conceito de 'VERDADEIRO' ou 'FALSO', onde o ? vai ser a verdadiro e o : falso. Tudo irá depende da questão para esse resultado. (Eu não entendi muito bem essa parte, mas espero entende com o tempo e experiencia.) È uma questão parecida com IF e ELSE.
+
+//_______________________________________________________________________
