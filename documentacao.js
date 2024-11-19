88bc92b2f2ec3982f60c1bc8d0c6ae474a9e8a0c
Isabella Ferreira da Silva <161853096+IsabellaFerreira03@users.noreply.github.com>
2024-11-19 11:54:38 -0300

    commit

--- documentacao.js
+++ documentacao.js
@@ -1,83 +1,88 @@
-// OS OPOERADORES NUMERICOS EM JS;
-// + para a soma
-// - menos para a subtração
-// * para a multiplicação
-// / para a divisão real
-// % para a divisão inteira
-// ** para achar o quadrado do numero.
+/* 
 
-// Um lembrete é que, uma ordem de operadores sempre será realizada primeiro, idependente dos que forem colocados primeiro, para os dar prioridade e manter a ordem desejada, deve os ccolocar em ()
+OS OPOERADORES NUMERICOS EM JS;
+ + para a soma
+ - menos para a subtração
+ * para a multiplicação
+ / para a divisão real
+ % para a divisão inteira
+** para achar o quadrado do numero.
 
-// A ordem de precedência dos operadores sempre será: () , **
+ Um lembrete é que, uma ordem de operadores sempre será realizada primeiro, idependente dos que forem colocados primeiro, para os dar prioridade e manter a ordem desejada, deve os ccolocar em ()
 
-//  * , / , % , tem a mesma precendência, nesse caso, quem aparecer primeiro, será feito primeiro.
+ A ordem de precedência dos operadores sempre será: () , **
 
-// e por último, ele fará + e -
+  * , / , % , tem a mesma precendência, nesse caso, quem aparecer primeiro, será feito primeiro.
 
-//_______________________________________________________________________
+ e por último, ele fará + e -
 
-// OPERADORES ATRIBUIÇÃO
-// As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
+_______________________________________________________________________
 
-// E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Mas isso será visto e aprendido melhor mais para frente.
+--OPERADORES ATRIBUIÇÃO
+ As variaveis são como 'as vagas de um carro' porque sempre que sai uma, vem outra, se N valia 3, e foi feito que N = N + 4, N tera o valor atribuido para o novo valor desse resultado, e assim por diante. Isso se chama auto-atribuição. Isso só funciona caso a mesma variavel receba ela mesma.
 
-//_______________________________________________________________________
+ E tambêm temos o n++, que adiciona um numero a mais, se antes era 1, passa a valer 2. E o mesmo vale para o n--. Mas tambêm existe outra vertente que é --n, e ++n, que ja adiciona o valor no momento exato, diferente de n++ que leva um comando a mais para ser efetuado. Mas isso será visto e aprendido melhor mais para frente.
 
-// MACETE: O comadno typeof mostra qual o tipo do dado que você quer ver.
+_______________________________________________________________________
 
-//_______________________________________________________________________
+MACETE: O comadno typeof mostra qual o tipo do dado que você quer ver.
 
-// OPERADORES RELACIONAIS
-// São os famosos:
-// > = Maior
-// < = menor
-// >= = maior ou igual
-// <= = menor ou igual
-// == = igual
-// != = diferente
+_______________________________________________________________________
 
-//_______________________________________________________________________
+--OPERADORES RELACIONAIS
+São os famosos:
+ > = Maior
+ < = menor
+ >= = maior ou igual
+ <= = menor ou igual
+ == = igual
+ != = diferente
 
-// OPERADORES DE IDENTIDADE
-// === = é quando é o mesmo valor e tipo de dado. Se forem iguais ele mostrara 'true'. Exemplo, separar string de numero.
+_______________________________________________________________________
 
-// !== = esse é o desigual restrito, que mostra se os tipos são didferentes, oU que tenham o mesmo valor. Se forem desiguais ele mostrata 'true' como exemplo, separar string de numero.
+--OPERADORES DE IDENTIDADE
+ === = é quando é o mesmo valor e tipo de dado. Se forem iguais ele mostrara 'true'. Exemplo, separar string de numero.
 
-//_______________________________________________________________________
+ !== = esse é o desigual restrito, que mostra se os tipos são didferentes, oU que tenham o mesmo valor. Se forem desiguais ele mostrata 'true' como exemplo, separar string de numero.
+
+_______________________________________________________________________
 
 
-// OPERADORES LÓGICOS
-// ! = negaçaõ (ou vai dar false ou true)
-// && = conjunção (Se tiver algum false na linha, será false)
-// || disjunção (se tive algum true na linnham será true.)
+--OPERADORES LÓGICOS
+! = negaçaõ (ou vai dar false ou true)
+&& = conjunção (Se tiver algum false na linha, será false)
+|| disjunção (se tive algum true na linnham será true.)
 
-// Se em uma linha forem colocados operadores aritmeticos, releacionais e lógicos, a ordem será, (aritmeticos, relacionais e lógicos.) 
+ Se em uma linha forem colocados operadores aritmeticos, releacionais e lógicos, a ordem será, (aritmeticos, relacionais e lógicos.) 
 
-// E caso a ordem de execução da conta seja (!, &&, ||) será feita na ordem de !, &&, ||.
+ E caso a ordem de execução da conta seja (!, &&, ||) será feita na ordem de !, &&, ||.
 
-//_______________________________________________________________________
+_______________________________________________________________________
 
-// ORDEM DE PRECEDENCIA DESSA BAGAÇA TODA!!!
-// aritmeticos
-// relacionais
-// lógicos
+ ORDEM DE PRECEDENCIA DESSA BAGAÇA TODA!!!
+ aritmeticos
+ relacionais
+ lógicos
 
-//_______________________________________________________________________
+_______________________________________________________________________
 
-// TERNÁRIO
-// ?
-// :
-// Aqui podemos ver o conceito de 'VERDADEIRO' ou 'FALSO', onde o ? vai ser a verdadiro e o : falso. Tudo irá depende da questão para esse resultado. (Eu não entendi muito bem essa parte, mas espero entende com o tempo e experiencia.) È uma questão parecida com IF e ELSE.
+ TERNÁRIO
+ ?
+ :
+ Aqui podemos ver o conceito de 'VERDADEIRO' ou 'FALSO', onde o ? vai ser a verdadiro e o : falso. Tudo irá depende da questão para esse resultado. (Eu não entendi muito bem essa parte, mas espero entende com o tempo e experiencia.) È uma questão parecida com IF e ELSE.
 
-//_______________________________________________________________________
+_______________________________________________________________________
 
-// COMANDOS.
+ COMANDOS.
 
-// window.alert() Cria uma janela com um aviso para o usuario na tela do site logo no inicio da entada ou em algum outro momento caso seja configurado.
+ window.alert('') Cria uma janela com um aviso para o usuario na tela do site logo no inicio da entada ou em algum outro momento caso seja configurado.
 
-// window.confirm() Cria uma janela com uma pergunta e irá dar dois botões, uma para sim e uma para não para respinder a pergunta na tela.
+ window.confirm('') Cria uma janela com uma pergunta e irá dar dois botões, uma para sim e uma para não para respinder a pergunta na tela.
 
-// window.prompt() Cria uma janela onde terá uma caixinha de texto para o usuario preencher cm a informção que foi pedida.
+ window.prompt('') Cria uma janela onde terá uma caixinha de texto para o usuario preencher cm a informção que foi pedida.
+
+
+*/
 
 
 
